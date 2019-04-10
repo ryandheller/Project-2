1af96d9701d916ae302a7e19bd2040a3632f2c40
rredundant <jarred.armijo@gmail.com>
2019-04-10 14:45:44 -0600

    added files

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
+import pymongo
+from pymongo import MongoClient
+import json
+from bson import json_util
+from bson.json_util import dumps
 
 app = Flask(__name__)
+
+
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+DBS_NAME = 'NYT_best_sellers'
+COLLECTION_NAME = 'info'
+FIELDS = {'Kindle': True, 'title': True, 'Audio': True, 'Harcover': True, 'Paperback': True, 'rank': True, 'author': True, 'date': True, '_id': False}
+
 
 @app.route("/")
 def index():
@@ -22,6 +35,18 @@ def titleSearch():
 @app.route("/price")
 def price():
     return render_template("price.html")
+
+@app.route("/nyt/jsondata")
+def nyt_jsondata():
+    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+    collection = connection[DBS_NAME][COLLECTION_NAME]
+    projects = collection.find(projection=FIELDS)
+    json_projects = []
+    for project in projects:
+        json_projects.append(project)
+    json_projects = json.dumps(json_projects, default=json_util.default)
+    connection.close()
+    return json_projects
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000,debug=True)
---
+++ static/js/charts.js
@@ -0,0 +1,19 @@
+// $.getJSON("{{url_for('/nyt/jsondata/')}}", function(data) {
+//     console.log(data)
+// })
+
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = 'json';
+
+    xhr.onload = function() {
+        var status = xhr.status;
+        if (status == 200) {
+            callback(null, response);
+        } else {
+            callback(status);
+        }
+    };
+    xhr.send(); 
+}
