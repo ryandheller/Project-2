33a0255d24a3b28e0264f140699fc0026cbf9ce9
ryandheller <hockeyman1_4@msn.com>
2019-04-09 18:25:55 -0600

    db upload

---
+++ get_amazon_info.py
@@ -0,0 +1,71 @@
+import requests
+import pandas as pd
+import re
+from bs4 import BeautifulSoup
+from requests_html import HTMLSession, user_agent
+from tqdm import tqdm
+import subprocess
+from datetime import datetime, timedelta
+import csv
+import os
+import glob
+import numpy as np
+import pickle
+
+# function to read html and gather the prices for each book in the htmlFolder
+price_dict = []
+count = 0
+def read_html_find_price(html_file):
+    with open(html_file, 'r', encoding='utf-8') as f:
+        soup = BeautifulSoup(f, "html.parser")
+    try:
+        prices = soup.find('ul', {"class": "a-unordered-list a-nostyle a-button-list a-horizontal"})\
+        .find_all("span", {"class": "a-button-inner"})
+    except Exception as e:
+        print('no soup for u', e, html_file) 
+    results = {}
+    strip_name = html_file.split('\\')[-1].strip()
+    name = strip_name.split('.')[0].strip()
+    for p in prices:
+        if "Kindle" in p.get_text():
+            try:
+                result = re.findall('\$\d{1,}\.\d{1,}', p.get_text())
+                results['Kindle'] = result[0].strip('[')
+            except: ''
+        elif "Hardcover" in p.get_text():
+            try:
+                result = re.findall('\$\d{1,}\.\d{1,}', p.get_text())
+                results['Hardcover'] = result[0].strip('[')
+            except: ''
+        elif "Paperback" in p.get_text():
+            try:
+                result = re.findall('\$\d{1,}\.\d{1,}', p.get_text())
+                results['Paperback'] = result[0].strip('[')
+            except: ''
+        elif "Audio" in p.get_text(): 
+            try:
+                result = re.findall('\$\d{1,}\.\d{1,}', p.get_text())
+                results['Audio'] = result[0].strip('[')
+            except: ''
+        results['Title'] = name
+    price_dict.append(results)
+    count += 1
+    print(count)
+    
+# only run the function on valid HTML files (some amazon links no longer exist (less than 5%))
+existing_files = [x for x in glob.glob('htmlFolder/*.html')]
+for files in existing_files:
+    if os.path.getsize(files) > 2500:
+        read_html_find_price(files)
+    else:
+        'ignore'
+    
+
+#pickle the results
+with open('amazon_prices1.pkl', 'wb') as f:
+    pickle.dump(price_dict, f, pickle.HIGHEST_PROTOCOL)
+
+with open('amazon_prices1.pkl','rb') as handle:
+    b = pickle.load(handle)
+print(price_dict == b)
+
--- nytimes_api.py
+++ nytimes_api.py
@@ -5,16 +5,26 @@ from bs4 import BeautifulSoup
 from requests_html import HTMLSession, user_agent
 from tqdm import tqdm
 import subprocess
+import time
 from datetime import datetime, timedelta
+import csv
+import json
+import glob
+import numpy as np
+import pickle
 
+#access token for api
 api_key = "REVeQu6ErJR69xuO8n21kyuUA0Xt4lV1"
+# first published date online
 starts_date = "02/13/11"
+#convert to Y-M-D
 start_date = datetime.strptime(starts_date, "%m/%d/%y")
-# yo = star_date.strftime('%Y-%m-%d')
+# create a list of list publication dates
 date_list=[]
 while start_date <= datetime.today():
     date_list.append(start_date.strftime('%Y-%m-%d'))
     start_date = (start_date + timedelta(days=7))
+# take dates to create a list of api urls for all dates
 def make_url(date):
     url = f"https://api.nytimes.com/svc/books/v3/lists/{date}/combined-print-and-e-book-fiction.json?api-key={api_key}"
     return url
@@ -23,24 +33,26 @@ for date in date_list:
     api_url = make_url(date)
     api_urls.append(api_url)
 
-book_list = []
-for api in api_urls[:5]:
-    resps = requests.get(api)
-    data = resps.json()
-    books = data.get('results').get('books')
-    book_list.append(books)
-print(book_list[0])
-# dicts=[]
-# amazon_urls = []
-# for book in books:
-#         rank = book.get('rank')
-#         title = book.get('title')
-#         author = book.get('author')
-#         amazon_url = book.get('amazon_product_url')
-#         book_dict={"rank":rank,"title":title,"author":author,"amazon_url":amazon_url}
-#         dicts.append(book_dict)
-#         amazon_urls.append(amazon_url)
-print(dicts[0])
-# https://api.nytimes.com/svc/books/v3/lists.json?list=combined-print-and-e-book-fiction&published-date=2019-03-30&api-key=[YOUR_API_KEY]
-# https://api.nytimes.com/svc/books/v3/lists/2011-02-13 00:00:00/combined-print-and-e-book-fiction.json?api-key=REVeQu6ErJR69xuO8n21kyuUA0Xt4lV1
-# https://api.nytimes.com/svc/books/v3/lists/2019-03-03/combined-print-and-e-book-fiction.json?api-key=REVeQu6ErJR69xuO8n21kyuUA0Xt4lV1
+# download the api files
+def download_api_files(url):
+    data = requests.get(url).json()
+    fullname = "api_download\ "+ url.split('/')[7] + ".json"
+    with open(fullname, 'w') as outfile:  
+        json.dump(data, outfile)
+
+#only download new api files
+api_list = [x for x in glob.glob('api_download/*.json')]
+downloaded_dates = []
+for api in api_list:
+    date = (api.split('\ ')[1].split('.')[0])
+    downloaded_dates.append(date)
+
+for url in api_urls:
+    url_date = url.split('/')[7]
+    if url_date in downloaded_dates:
+        'alreaady downloaded'
+    else:
+        print(url_date)
+        download_api_files(url)
+
+    
